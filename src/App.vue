5af571d9526468d1541972206e5836bb8e7e23b2
Hyman <hy1995@gmail.com>
2018-02-28 15:01:11 +1100

    make page count for file works

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,13 @@
     <input type="file" name="files" id="files" multiple @change="processFile($event)">
     <el-table :data="tableData" style="width: 100%">
       <el-table-column prop="name" label="文件名" width="180"></el-table-column>
-      <el-table-column prop="pages" label="页数" width="180">
+      <el-table-column prop="pageCount" label="页数" width="180">
+      </el-table-column>
+      <el-table-column prop="preview" label="预览" width="180">
         <template slot-scope="scope">
-          <pdf class="" :src="scope.row.address"></pdf>
+          <pdf class="preview" :src="scope.row.address" @num-pages="updatePageCount($event, scope.$index)"></pdf>
         </template>
       </el-table-column>
-      <el-table-column prop="address" label="地址"></el-table-column>
     </el-table>
   </div>
 </template>
@@ -35,14 +36,24 @@ export default {
       const URL = window.URL || window.webkitURL
       for (let item of this.fileList) {
         item.address = URL.createObjectURL(item)
+        item.pageCount = 0
         this.tableData.push(item)
       }
-      console.log(this.tableData)
+    },
+    updatePageCount(event, index) {
+      if (event) {
+        let newItem = this.tableData[index]
+        newItem.pageCount = event
+        this.$set(this.tableData, index, newItem)
+      }
     }
   }
 }
 </script>
 
 <style>
-
+.preview {
+  display: inline-block;
+  height: 200px;
+}
 </style>
